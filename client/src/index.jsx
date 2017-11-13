f039387d8f576778414fe9254a6c49509d16fd9c
Kaylin <kaylin.collins68@gmail.com>
2017-11-12 19:40:53 -0800

    does a get request for intial data

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,26 +3,32 @@ import ReactDOM from 'react-dom';
 import groceryList from '../../database/data.js';
 import GroceryList from './components/GroceryList.jsx';
 import AddGrocery from './components/AddGrocery.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [  
-        {id: 1, quantity: 5, description: "frozen pizza"},
-        {id: 2, quantity: 10, description: "greek yogurt"},
-        {id: 3, quantity: 2, description: "wine"},
-        {id: 4, quantity: 1, description: "iced coffee"}
-      ]
+      list: []
     }
   }
 
   onClick(desc, quantity) {
-    console.log(desc, quantity);
     var newList = this.state.list.concat({id: 4, quantity: quantity, description: desc});
     this.setState({
       list: newList
     }, () => this.refs.AddGrocery.resetDefault()) 
+  }
+
+  handleData(data) {
+    this.setState({
+      list: data
+    })
+  }
+
+  componentDidMount() {
+    axios.get('/list')
+    .then((res) => this.handleData(res.data))
   }
 
   render () {
--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var data = require('../database/data');
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParse.json())
 
 app.listen(3000, function() {
   console.log('Server started and listening on port 3000');
 });
 
+app.get('/list', function(req, res) {
+  res.end(JSON.stringify(data.groceryList));
+})
+
+app.post('/list', function(req, res) {
+  console.log(req.data);
+  res.end();
+})
